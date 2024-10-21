89679f54a7e06eeebe032203da21ae6cded63cc0
DnlSnchz7 <90650229+DnlSnchz7@users.noreply.github.com>
2024-10-21 12:44:15 -0600

    Preoductos aleatorios en demanda
    Ya no se repiten productos
    Revisar seed*

--- api.py
+++ api.py
@@ -219,13 +219,17 @@ def fill_patio():
 # Create: Generar demanda y guardar en un CSV
 @app.route('/demanda/create/<int:seed>', methods=['POST'])
 def create_demanda(seed):
-    random.seed(seed)
+    random.seed(seed) #Revisar lo del seed****
     
     demanda = []
-    for i in range(20):  # Generaremos 20 productos
+    productos_usados = set()  # Set para almacenar los productos ya seleccionados
+
+    while len(productos_usados) < 20:  
         id_producto = random.randint(1, 56)
-        cantidad = random.randint(40, 2000)
-        demanda.append([id_producto, cantidad])
+        if id_producto not in productos_usados:
+            cantidad = random.randint(40, 2000)
+            demanda.append([id_producto, cantidad])
+            productos_usados.add(id_producto)  
 
     # Guardar en CSV
     with open(DEMANDA_CSV_PATH, mode='w', newline='') as file:
